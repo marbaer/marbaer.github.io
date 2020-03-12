8629b834327089096a8c37687e805e5de2710749
marbaer <57128523+marbaer@users.noreply.github.com>
2020-03-12 22:27:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+  var countDownDate = new Date(
+    "Apr 28, 2020 14:00:00").getTime();
+   
+   var x = setInterval(function() {
+    
+    var now = new Date().getTime();
+    
+    var distance = countDownDate - now;
+    
+    var days = Math.floor(distance / (
+     1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (
+     1000 * 60 * 60 * 24)) / (1000 *
+     60 * 60));
+    var minutes = Math.floor((distance %
+     (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance %
+     (1000 * 60)) / 1000);
+       
+     document.getElementById("days")
+     .innerHTML = days + "d ";
+     
+     document.getElementById("hours")
+     .innerHTML = hours +
+     "h " +
+     minutes + "m " + seconds + "s ";
+
+   }, 1000);
